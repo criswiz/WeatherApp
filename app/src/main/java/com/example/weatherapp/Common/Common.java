3fb0603f776ade5712ff887b36bfd885376ec6ba
eventifyorg <eventifyorg@gmail.com>
2020-11-23 08:29:13 +0000

    Initial Work Done on the weather app

---
+++ app/src/main/java/com/example/weatherapp/Common/Common.java
@@ -0,0 +1,7 @@
+package com.example.weatherapp.Common;
+
+public class Common {
+
+    public static final String APP_ID = "f4a9f44249df66ee9cfef7d98d195f91";
+
+}
---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    private CoordinatorLayout coordinatorLayout;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        coordinatorLayout = findViewById(R.id.root_view);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //REQUEST PERMISSION
+        tabLayout = findViewById(R.id.tabs);
+        viewPager = findViewById(R.id.view_pager);
+
+    }
+}
